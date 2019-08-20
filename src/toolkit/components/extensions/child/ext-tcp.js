599f70c7b7c5c4cbd89580b70db91e6c728e7c35
Sam Macbeth <sam@cliqz.com>
2019-08-20 15:54:05 +0200

    WIP: connect object

--- src/toolkit/components/extensions/child/ext-tcp.js
+++ src/toolkit/components/extensions/child/ext-tcp.js
@@ -1,101 +1,125 @@
 const { ExtensionError } = ExtensionUtils
 
-global.TCPSocket = class extends ExtensionAPI /*::<Host>*/ {
-  getAPI(context) {
-    class TCPClient {
-      /*::
-      opened:Promise<void>
-      closed:Promise<void>
-      */
-      constructor({ host, port, ssl, readyState }) {
-        this.host = host
-        this.port = port
-        this.ssl = ssl
-        this.readyState = readyState
-        // this.opened = new Promise((resolve) => {
-        //   if (this.readyState === "open") {
-        //     resolve();
-        //   } else {
-        //     this._onOpened = resolve;
-        //   }
-        // });
-        // this.closed = new Promise((resolve) => {
-        //   this._onClosed = resolve;
-        // });
-      }
-      get bufferedAmount() {
-        return derefSocket(this).bufferedAmount
-      }
-      write(buffer, byteOffset, byteLength) {
-        const socket = derefSocket(this)
-        if (socket.send(buffer, byteOffset, byteLength)) {
-          return voidPromise
-        } else {
-          return new context.cloneScope.Promise((resolve, reject) => {
-            socket.ondrain = () => resolve()
-            socket.onerror = ({ name, message }) =>
-              reject(new IOError(`${name}: ${message}`))
-          })
-        }
-      }
-      read() {
-        return context.wrapPromise(
-          new Promise((resolve, reject) => {
-            derefSocket(this).ondata = event => resolve(event.data)
+const exportClass = /*::<b, a:Class<b>>*/ (
+  scope /*:Object*/,
+  constructor /*:a*/
+) /*:a*/ => {
+  const clone = Cu.exportFunction(constructor, scope)
+  const unwrapped = Cu.waiveXrays(clone)
+  const prototype = Cu.waiveXrays(Cu.createObjectIn(scope))
+
+  const source = constructor.prototype
+  for (const key of Reflect.ownKeys(constructor.prototype)) {
+    if (key !== "constructor") {
+      const descriptor = Reflect.getOwnPropertyDescriptor(source, key)
+      Reflect.defineProperty(
+        prototype,
+        key,
+        Cu.waiveXrays(
+          Cu.cloneInto(descriptor, scope, {
+            cloneFunctions: true
           })
         )
-      }
-      suspend() {
-        derefSocket(this).suspend()
-      }
-      resume() {
-        derefSocket(this).resume()
-      }
-      close() {
-        derefSocket(this).close()
-        return voidPromise
-      }
-      closeImmediately() {
-        derefSocket(this).closeImmediately()
-        return voidPromise
-      }
-      upgradeToSecure() {
-        return derefSocket(this).upgradeToSecure()
-      }
+      )
+    }
+  }
+
+  Reflect.defineProperty(unwrapped, "prototype", {
+    value: prototype
+  })
+  Reflect.defineProperty(prototype, "constructor", {
+    value: unwrapped
+  })
+
+  return clone
+}
+
+const exportInstance = /*::<a:Object, b:a>*/ (
+  scope,
+  constructor /*:Class<b>*/,
+  properties /*::?:a*/
+) /*:b*/ => {
+  const instance /*:any*/ = properties
+    ? Cu.cloneInto(properties, scope)
+    : Cu.cloneInto({}, scope)
+  Reflect.setPrototypeOf(
+    Cu.waiveXrays(instance),
+    Cu.waiveXrays(constructor).prototype
+  )
+  return instance
+}
+
+global.TCPSocket = class extends ExtensionAPI /*::<Host>*/ {
+  getAPI(context) {
+    const connections = new Map()
+
+    const derefSocket = client => {
+      return connections.get(client.__id)
     }
 
-    class TCPServer {
-      /*::
-      connections:AsyncIterator<ClientSocket>
-      */
-      constructor({ localPort, id }) {
-        this.id = id
-        this.localPort = localPort
-      }
-      close() {
-        this.server.close()
-      }
-      // get closed() {
-      //   return this.server.closed
-      // }
-      // get localPort() {
-      //   return this.server.localPort
-      // }
-    }
-    const createTCPServer = server => {
-      const tcpServer = Cu.waiveXrays(Reflect.construct(TCPServer, [server]))
-      // Reflect.defineProperty(tcpServer, 'localPort', {
-      //   value() {
-      //     return server.localPort
-      //   }
-      // })
-      Reflect.defineProperty(tcpServer, "closed", {
-        get() {
-          return Promise.reject()
+    const TCPClient = exportClass(
+      context.cloneScope,
+      class TCPClient {
+        /*::
+        opened:Promise<void>
+        closed:Promise<void>
+        */
+        constructor() {
+          throw TypeError("Illegal constructor")
         }
-      })
-      return tcpServer
-    }
+        get host() {
+          return derefSocket(this).host
+        }
+        get port() {
+          return derefSocket(this).port
+        }
+        get ssl() {
+          return derefSocket(this).ssl
+        }
+        get readyState() {
+          return derefSocket(this).readyState
+        }
+        get bufferedAmount() {
+          return derefSocket(this).bufferedAmount
+        }
+        write(buffer, byteOffset, byteLength) {
+          const socket = derefSocket(this)
+          if (socket.send(buffer, byteOffset, byteLength)) {
+            return voidPromise
+          } else {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              socket.ondrain = () => resolve()
+              socket.onerror = ({ name, message }) =>
+                reject(new IOError(`${name}: ${message}`))
+            })
+          }
+        }
+        read() {
+          return context.wrapPromise(
+            new Promise((resolve, reject) => {
+              derefSocket(this).ondata = event => resolve(event.data)
+            })
+          )
+        }
+        suspend() {
+          derefSocket(this).suspend()
+        }
+        resume() {
+          derefSocket(this).resume()
+        }
+        close() {
+          derefSocket(this).close()
+          return voidPromise
+        }
+        closeImmediately() {
+          derefSocket(this).closeImmediately()
+          return voidPromise
+        }
+        upgradeToSecure() {
+          return derefSocket(this).upgradeToSecure()
+        }
+      }
+    )
 
     return {
       TCPSocket: {
@@ -114,15 +138,19 @@ global.TCPSocket = class extends ExtensionAPI /*::<Host>*/ {
             }
           }),
         connect: options =>
-          new Promise(async (resolve, reject) => {
+          new context.cloneScope.Promise(async (resolve, reject) => {
             try {
-              console.log("xxx", Object.keys(context))
               const parentServer = await context.childManager.callParentAsyncFunction(
                 "TCPSocket.connect",
                 [options]
               )
-              // console.log('xxx', parentServer);
-              resolve(new TCPClient(parentServer))
+              console.log("xxx", parentServer)
+              connections.set(parentServer.id, parentServer)
+
+              const client = exportInstance(context.cloneScope, TCPClient)
+              client.__id = parentServer.id
+
+              resolve(client)
             } catch (e) {
               reject(new ExtensionError(e))
             }
