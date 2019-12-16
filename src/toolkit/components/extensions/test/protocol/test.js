06566f5482065ee3873b18529cff740bdf9d399a
Sam Macbeth <sam@cliqz.com>
2019-12-16 16:56:03 +0100

    Add a protocol test.

--- src/toolkit/components/extensions/test/protocol/test.js
+++ src/toolkit/components/extensions/test/protocol/test.js
@@ -2,7 +2,7 @@
 
 const { test } = browser.test
 
-test("protocol", test => {
+test("protocol", async test => {
   const { protocol } = browser
   test.equal(typeof protocol, "object", "protocol API available")
   test.equal(
@@ -10,4 +10,15 @@ test("protocol", test => {
     "function",
     "has protocol.registerProtocol"
   )
+
+  protocol.registerProtocol("test", req => {
+    return new Response("hello world")
+  })
+
+  await new Promise(resolve => setTimeout(resolve, 500))
+  const req = await fetch("test://something")
+  const resp = await req
+  test.ok(resp.ok, "Can fetch protocol URL")
+  const text = await resp.text()
+  test.equal(text, "hello world", "Content matches")
 })
